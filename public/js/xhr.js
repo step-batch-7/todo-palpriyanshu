4791a7b7e8ca6f27ec13ecde4fe7d597620531f5
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-04 14:45:32 +0530

    added mechanism-- onClick on title the itemBlock will render
    there is a bug that for different title shares the same item Page
    data storing for items has to be handled

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,19 +1,48 @@
-const xhrRequest = function() {
-  const req = new XMLHttpRequest();
+const renderItems = function() {
+  const button = document.getElementById('addButton');
+  button.onclick = taskRequest;
+};
 
-  req.open('POST', '/template/todoPage.html');
-  req.setRequestHeader('content-type', 'application/json');
-
-  req.onload = function() {
-    if (req.status === 201) {
+const titleRequest = function() {
+  const title = document.getElementById('titlePlace').value;
+  const callBack = function() {
+    if (this.status === 201) {
       const textArea = document.createElement('div');
+      textArea.onclick = renderItems;
       textArea.classList.add('project');
       textArea.innerText = title;
       const index = document.getElementById('index');
       index.appendChild(textArea);
     }
   };
+  newRequest('POST', 'saveTitle', callBack, `title=${title}`);
+};
 
-  const title = document.getElementById('titlePlace').value;
-  req.send(`title=${title}`);
+const taskRequest = function() {
+  const task = document.getElementById('task').value;
+
+  // eslint-disable-next-line max-statements
+  const callBack = function() {
+    const textArea = document.getElementById('textArea');
+    const block = document.createElement('div');
+    block.classList.add('display');
+    const item = document.createElement('div');
+    item.classList.add('heading');
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkBox');
+    checkBox.classList.add('check');
+    block.appendChild(checkBox);
+    block.appendChild(item);
+    textArea.appendChild(block);
+    item.innerText = task;
+  };
+
+  newRequest('POST', 'saveTask', callBack, `task=${task}`);
+};
+
+const newRequest = function(method, url, callBack, reqMsg) {
+  const req = new XMLHttpRequest();
+  req.open(method, url);
+  req.onload = callBack;
+  req.send(reqMsg);
 };
