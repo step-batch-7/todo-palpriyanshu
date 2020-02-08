9fa59563c480b76a04306d28883494c97aa94bce
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-08 16:36:06 +0530

    added config file. Added resource folder for test in test folder

---
+++ config.js
@@ -0,0 +1,7 @@
+const {env} = require('process');
+
+const config = {
+  DATA_STORE: env.DATA_STORE
+};
+
+module.exports = config;
--- test/responseTest.js
+++ test/responseTest.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+
 const app = require('../lib/handler.js');
 const STATUS_CODES = require('../lib/statusCodes.js');
 
