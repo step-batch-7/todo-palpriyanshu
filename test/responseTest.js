7ebcc358f9a3435c36dd1dfcbc2e3f249b2a4bed
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-08 16:13:19 +0530

    added test for getMethod for css and png files

--- test/responseTest.js
+++ test/responseTest.js
@@ -22,11 +22,42 @@ describe('GET', function() {
         .expect(STATUS_CODES.ok, done);
     });
 
+    it('should respond with landingPage when url is "/index.html"', function(done) {
+      request(app.serveRequest.bind(app))
+        .get('/index.html')
+        .set('Accept', '*/*')
+        .expect('content-type', /html/)
+        .expect('content-length', '602')
+        .expect(STATUS_CODES.ok, done);
+    });
+
     it('should respond with todoPage when url is "/template/todoPage.html"', function(done) {
       request(app.serveRequest.bind(app))
         .get('/')
         .set('Accept', '*/*')
         .expect('content-type', /html/)
+        .expect(STATUS_CODES.ok, done);
+    });
+  });
+
+  context('request for css file', function() {
+    it('should respond with styleSheet when url is "/css/style.css"', function(done) {
+      request(app.serveRequest.bind(app))
+        .get('/css/style.css')
+        .set('Accept', '*/*')
+        .expect('content-type', /css/)
+        .expect('content-length', '1900')
+        .expect(STATUS_CODES.ok, done);
+    });
+  });
+
+  context('request for png file', function() {
+    it('should respond with image when url is "/images/folder.png"', function(done) {
+      request(app.serveRequest.bind(app))
+        .get('/images/folder.png')
+        .set('Accept', '*/*')
+        .expect('content-type', /png/)
+        .expect('content-length', '4616')
         .expect(STATUS_CODES.ok, done);
     });
   });
