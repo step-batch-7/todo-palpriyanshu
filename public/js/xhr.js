df00add95022cc7dae06cafe876a90b56f0d7c56
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-11 19:32:38 +0530

    [priyanshu | santhosh]
    added mechanism to edit title
    added image of pencil in images
    added handler and response for editTitle
    
    there is a bug. After press enter the new Line will come in titleBox

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -6,117 +6,137 @@ const show = selector => getElement(selector).classList.remove('hidden');
 
 const erase = id => document.getElementById(id).remove();
 
-const taskAsHtml = function(task) {
-  const {id, name, done} = task;
-  const isChecked = done ? 'checked' : '';
-  return `<div class="display" id="${id}">
-    <input type="checkBox" class="check" onclick="updateStatus()" ${isChecked}>
+const tasksAsHtml = function (tasks) {
+  return tasks.map(({ id, name, done }) => {
+    return `<div class="display" id="${id}">
+    <input type="checkBox" class="check" onclick="updateStatus()" ${done ? 'checked' : ''}>
     <div class="heading">${name}</div>
     <img src="../images/minus.png" class="titleImg minus" onclick="deleteTask()">
   </div>`;
+  })
+    .join('\n');
 };
 
-const todoBlockAsHtml = function(todoId) {
+const todoBlockAsHtml = function (id, title, tasks) {
   return `
-  <div class="myTasks" id="c.${todoId}">
+  <div class="myTasks" id="c.${id}">
+    <div class="display">
+      <div class="title" contenteditable="true" onkeyup="editTitle('${id}',this)"> ${title}</div>
+      <div class="editIcon">
+        <img src="../images/pencil.png" alt="image not found"/>
+      </div>
+    </div>
     <div id="taskInput" class="display">
       <input placeholder="add Task" type="text" id="task" required />
-      <img src="../images/plus.png" class="titleImg" id="addButton" onclick = "addNewTask('${todoId}')"/>
+      <img src="../images/plus.png" class="titleImg" id="addButton" onclick = "addNewTask('${id}')"/>
     </div>
     <br />
     <br />
-    <div id="todo"></div>
+    <div id="todo">${tasksAsHtml(tasks)} </div>
   </div>`;
 };
 
-const displayTodo = function(todoId) {
-  const myAllTasks = getElement('#myAllTasks');
-  myAllTasks.innerHTML = todoBlockAsHtml(todoId);
-  const callBack = function() {
+const displayTodo = function (todoId) {
+  const callBack = function () {
     if (this.status === 201) {
-      const tasks = JSON.parse(this.response).tasks;
-      const todo = getElement('#todo');
-      todo.innerHTML = tasks.map(task => taskAsHtml(task)).join('\n');
+      const { id, title, tasks } = JSON.parse(this.response);
+      const myAllTasks = getElement('#myAllTasks');
+      myAllTasks.innerHTML = todoBlockAsHtml(id, title, tasks);
     }
   };
-  newRequest('POST', 'loadTask', callBack, {todoId});
+  newRequest('POST', 'loadTask', callBack, { todoId });
 };
 
-const addToTodoList = function({id, title}) {
+const addToTodoList = function ({ id, title }) {
   const todoLists = getElement('#allTodos');
   const html = `<div class="project" id="${id}" onclick="displayTodo('${id}')">
-     ${title} </div>`;
+     ${title} 
+     </div>`;
   todoLists.innerHTML += html;
 };
 
-const createTodo = function() {
+const createTodo = function () {
   const textBox = getElement('#titlePlace');
   const title = textBox.value;
   textBox.value = '';
-  const callBack = function() {
+  const callBack = function () {
     if (this.status === 201) {
       addToTodoList(JSON.parse(this.response));
     }
   };
-  newRequest('POST', 'saveTitle', callBack, {title});
+  newRequest('POST', 'saveTitle', callBack, { title });
 };
 
-const deleteTodo = function(todoId) {
+const deleteTodo = function (todoId) {
   const callBack = () => {
     getElement('#todo').innerText = '';
     hide('.myTasks');
     erase(`${todoId}`);
   };
-  newRequest('POST', 'deleteAllTodo', callBack, {todoId});
+  newRequest('POST', 'deleteAllTodo', callBack, { todoId });
   hide('.dialogBox');
 };
 
-const showDeleteDialogBox = function() {
+const showDeleteDialogBox = function () {
   const id = event.target.id;
   show('.dialogBox');
   getElement('.delete').setAttribute('onclick', `deleteTodo('${id}')`);
   getElement('.cancel').setAttribute('onclick', 'hide(".dialogBox")');
 };
 
-const addNewTask = function(titleId) {
+const addNewTask = function (titleId) {
   const todoBlock = getElement('#todo');
   const textBox = getElement('#task');
   const name = textBox.value;
   textBox.value = '';
-  const callBack = function() {
+  const callBack = function () {
     const task = JSON.parse(this.response);
     todoBlock.innerHTML += taskAsHtml(task);
   };
 
-  newRequest('POST', 'saveTask', callBack, {name, titleId});
+  newRequest('POST', 'saveTask', callBack, { name, titleId });
 };
 
-const deleteTask = function() {
+const deleteTask = function () {
   const myTasks = getElement('.myTasks');
   const todoId = myTasks.id.split('.').pop();
   const taskId = event.target.parentElement.id;
   const callBack = () => erase(`${taskId}`);
-  newRequest('POST', 'deleteTask', callBack, {todoId, taskId});
+  newRequest('POST', 'deleteTask', callBack, { todoId, taskId });
 };
 
-const updateStatus = function() {
+const updateStatus = function () {
   const myTasks = getElement('.myTasks');
   const titleId = myTasks.id.split('.').pop();
   const taskId = event.target.parentElement.id;
-  newRequest('POST', 'updateTaskStatus', true, {titleId, taskId});
+  newRequest('POST', 'updateTaskStatus', true, { titleId, taskId });
 };
 
-const filterTodo = function() {
+const editTitle = function (todoId, titleDivision) {
+  const title = titleDivision.innerText;
+  const callBack = function () {
+    const newTitle = JSON.parse(this.response);
+    const title = getElement('.title');
+    const todo = getElement(`#${todoId}`);
+    title.innerText = newTitle;
+    todo.innerText = newTitle;
+  };
+  if (event.key === 'Enter' && title.innerText !== '') {
+    newRequest('POST', 'editTitle', callBack, { todoId, title });
+  }
+};
+const filterTodo = function () {
   const searchValue = event.target.value;
-  const callback = function() {
+  const callback = function () {
     const matchedValue = JSON.parse(this.response);
+    console.log(this.response);
     const form = getElement('#myAllTasks');
     form.innerHTML = '';
   };
-  newRequest('POST', 'filterTodo', callback, {searchValue});
+  newRequest('POST', 'filterTodo', callback, { searchValue });
 };
 
-const newRequest = function(method, url, callBack, reqMsg) {
+const newRequest = function (method, url, callBack, reqMsg) {
   const req = new XMLHttpRequest();
   req.open(method, url);
   req.setRequestHeader('content-type', 'application/json');
@@ -124,7 +144,7 @@ const newRequest = function(method, url, callBack, reqMsg) {
   req.send(JSON.stringify(reqMsg));
 };
 
-const renderIndex = function() {
+const renderIndex = function () {
   const index = getElement('#index');
   index.classList.toggle('hidden');
 };
