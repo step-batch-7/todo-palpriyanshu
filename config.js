395e4f1afa3eb201a652bc7a741d2bdfea3f32b4
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-13 14:08:16 +0530

    added USER_STORE in configuartion file and added environment variable for USER_STORE in package.json

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-const {env} = require('process');
+const { env } = require('process');
 
 const config = {
   DATA_STORE: env.DATA_STORE,
+  USERS_STORE: env.USER_STORE,
   SAMPLE_TODO: env.SAMPLE_TODO
 };
 
