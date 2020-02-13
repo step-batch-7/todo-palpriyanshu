8a5439b563c8fc8dfe5ed58a52511a92e5a72ac3
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-02-13 20:19:33 +0530

    changed server to express module extracted env variables to different files deleted app.js and handler.js

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,6 +1,7 @@
 const getElement = selector => document.querySelector(selector);
 
-const getChildElement = (parentElement, selector) => parentElement.querySelector(selector);
+const getChildElement = (parentElement, selector) =>
+  parentElement.querySelector(selector);
 
 const hide = selector => getElement(selector).classList.add('hidden');
 
@@ -11,22 +12,25 @@ const erase = id => document.getElementById(id).remove();
 const highlightText = (text, searchText) => {
   const highlightedText = `<span class="highlight">${searchText}</span>`;
   return text.replace(searchText, highlightedText);
-}
+};
 
-const tasksAsHtml = function (todoId, tasks, searchText) {
-  return tasks.map(({ id, name, done }) => {
-    return `<div class="display todoTask" id="${id}">
-    <input type="checkBox" class="check" onclick="updateStatus()" ${done ? 'checked' : ''}>
+const tasksAsHtml = function(todoId, tasks, searchText) {
+  return tasks
+    .map(({id, name, done}) => {
+      return `<div class="display todoTask" id="${id}">
+    <input type="checkBox" class="check" onclick="updateStatus()" ${
+      done ? 'checked' : ''
+    }>
     <div class="taskName" contenteditable="true" onblur="editTask('${todoId}','${id}',this)">
       ${highlightText(name, searchText)}
     </div>
     <img src="../images/minus.png" class="titleImg minus" onclick="deleteTask()">
   </div>`;
-  })
+    })
     .join('\n');
 };
 
-const todoBlockAsHtml = function ({ id, title, tasks }, searchText) {
+const todoBlockAsHtml = function({id, title, tasks}, searchText) {
   return `
   <div class="myTasks" id="c.${id}">
     <div class="display">
@@ -47,18 +51,18 @@ const todoBlockAsHtml = function ({ id, title, tasks }, searchText) {
   </div>`;
 };
 
-const displayTodo = function (todoId) {
-  const callBack = function () {
-    if (this.status === 201) {
+const displayTodo = function(todoId) {
+  const callBack = function() {
+    if (this.status === 200) {
       const todos = JSON.parse(this.response);
       const myAllTasks = getElement('#myAllTasks');
       myAllTasks.innerHTML = todos.map(todo => todoBlockAsHtml(todo));
     }
   };
-  newRequest('POST', 'loadTask', callBack, { todoId });
+  newRequest('POST', 'loadTask', callBack, {todoId});
 };
 
-const addToTodoList = function ({ id, title }) {
+const addToTodoList = function({id, title}) {
   const todoLists = getElement('#allTodos');
   const html = `<div class="todoTitle" id="${id}" onclick="displayTodo('${id}')">
      ${title} 
@@ -66,67 +70,67 @@ const addToTodoList = function ({ id, title }) {
   todoLists.innerHTML += html;
 };
 
-const createTodo = function () {
+const createTodo = function() {
   const textBox = getElement('#titlePlace');
   const title = textBox.value;
   textBox.value = '';
-  const callBack = function () {
-    if (this.status === 201) {
+  const callBack = function() {
+    if (this.status === 200) {
       addToTodoList(JSON.parse(this.response));
     }
   };
-  newRequest('POST', 'saveTitle', callBack, { title });
+  newRequest('POST', 'saveTitle', callBack, {title});
 };
 
-const deleteTodo = function (todoId) {
+const deleteTodo = function(todoId) {
   const callBack = () => {
     getElement('#todo').innerText = '';
     hide('.myTasks');
     erase(`${todoId}`);
   };
-  newRequest('DELETE', 'deleteAllTodo', callBack, { todoId });
+  newRequest('DELETE', 'deleteAllTodo', callBack, {todoId});
   hide('.dialogBox');
 };
 
-const showDeleteDialogBox = function () {
+const showDeleteDialogBox = function() {
   const id = event.target.id;
   show('.dialogBox');
   getElement('.delete').setAttribute('onclick', `deleteTodo('${id}')`);
   getElement('.cancel').setAttribute('onclick', 'hide(".dialogBox")');
 };
 
-const addNewTask = function (todoId) {
+const addNewTask = function(todoId) {
   const todoDivision = document.getElementById(`c.${todoId}`);
-  const taskDivision = getChildElement(todoDivision, "#todo");
-  const taskInput = getChildElement(todoDivision, '#taskInput')
-  const textBox = getChildElement(taskInput, ".task");
+  const taskDivision = getChildElement(todoDivision, '#todo');
+  const taskInput = getChildElement(todoDivision, '#taskInput');
+  const textBox = getChildElement(taskInput, '.task');
   const name = textBox.value;
   textBox.value = '';
-  const callBack = function () {
+  const callBack = function() {
     const task = JSON.parse(this.response);
     taskDivision.innerHTML += tasksAsHtml(todoId, [task]);
   };
 
-  newRequest('POST', 'saveTask', callBack, { name, todoId });
+  newRequest('POST', 'saveTask', callBack, {name, todoId});
 };
 
-const deleteTask = function () {
+const deleteTask = function() {
   const myTasks = getElement('.myTasks');
   const todoId = myTasks.id.split('.').pop();
   const taskId = event.target.parentElement.id;
   const callBack = () => erase(`${taskId}`);
-  newRequest('DELETE', 'deleteTask', callBack, { todoId, taskId });
+  newRequest('DELETE', 'deleteTask', callBack, {todoId, taskId});
 };
 
-const updateStatus = function () {
+const updateStatus = function() {
   const myTasks = getElement('.myTasks');
   const titleId = myTasks.id.split('.').pop();
   const taskId = event.target.parentElement.id;
-  newRequest('PATCH', 'updateTaskStatus', true, { titleId, taskId });
+  newRequest('PATCH', 'updateTaskStatus', true, {titleId, taskId});
 };
 
-const editTitle = function (todoId, titleDivision) {
-  const callBack = function () {
+const editTitle = function(todoId, titleDivision) {
+  const callBack = function() {
     const newTitle = JSON.parse(this.response);
     const title = getElement('.title');
     const todo = getElement(`#${todoId}`);
@@ -134,31 +138,33 @@ const editTitle = function (todoId, titleDivision) {
     todo.innerText = newTitle;
   };
   const title = titleDivision.innerText;
-  newRequest('POST', 'editTitle', callBack, { todoId, title });
+  newRequest('POST', 'editTitle', callBack, {todoId, title});
 };
 
-const editTask = function (todoId, taskId, taskDivision) {
-  const callBack = function () {
+const editTask = function(todoId, taskId, taskDivision) {
+  const callBack = function() {
     const newTask = JSON.parse(this.response);
     const taskDivision = getElement(`#${taskId}`);
-    const taskName = getChildElement(taskDivision, '.heading');
+    const taskName = getChildElement(taskDivision, '.taskName');
     taskName.innerText = newTask;
   };
   const name = taskDivision.innerText;
-  newRequest('POST', 'editTask', callBack, { todoId, taskId, name });
-}
-
-const filterTodo = function () {
-  const searchValue = event.target.value;
-  const callback = function () {
-    const matchedTodos = JSON.parse(this.response);
-    const form = getElement('#myAllTasks');
-    form.innerHTML = matchedTodos.map(todo => todoBlockAsHtml(todo, searchValue)).join('');
-  };
-  newRequest('POST', 'filterTodo', callback, { searchValue });
+  newRequest('POST', 'editTask', callBack, {todoId, taskId, name});
 };
 
-const newRequest = function (method, url, callBack, reqMsg) {
+const filterTodo = function() {
+  const searchValue = event.target.value;
+  const callback = function() {
+    const matchedTodos = JSON.parse(this.response);
+    const form = getElement('#myAllTasks');
+    form.innerHTML = matchedTodos
+      .map(todo => todoBlockAsHtml(todo, searchValue))
+      .join('');
+  };
+  newRequest('POST', 'filterTodo', callback, {searchValue});
+};
+
+const newRequest = function(method, url, callBack, reqMsg) {
   const req = new XMLHttpRequest();
   req.open(method, url);
   req.setRequestHeader('content-type', 'application/json');
@@ -166,7 +172,7 @@ const newRequest = function (method, url, callBack, reqMsg) {
   req.send(JSON.stringify(reqMsg));
 };
 
-const renderIndex = function () {
+const renderIndex = function() {
   const index = getElement('#index');
   index.classList.toggle('hidden');
 };
--- server.js
+++ server.js
@@ -1,14 +1,62 @@
-const {stdout, stderr} = require('process');
-const {Server} = require('http');
-const app = require('./lib/handler.js');
+const {stdout} = require('process');
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const {
+  serveTodoPage,
+  serveLoginPage,
+  loginUser,
+  serveStaticFiles,
+  saveTodo,
+  deleteTodo,
+  saveTask,
+  deleteTask,
+  updateTaskStatus,
+  loadTask,
+  editTitle,
+  editTask,
+  filterTodo,
+  notFound,
+  methodNotAllowed
+} = require('./lib/responses');
 
-const main = function(port = 4000) {
-  const server = new Server(app.serveRequest.bind(app));
-  server.on('clientError', err => stderr.write(`server error, ${err}\n`));
-  server.on('listening', () => {
-    stdout.write(`server listening to ${JSON.stringify(server.address())}\n`);
-  });
-  server.listen(port);
+const port = 4000;
+const app = express();
+
+const hasFields = (...fields) => {
+  return function(req, res, next) {
+    if (fields.every(field => field in req.body)) {
+      next();
+    }
+    res.statusCode = 400;
+    res.end('bad request');
+  };
 };
 
-main(process.argv[2]);
+app.use(morgan('tiny'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/todo', serveTodoPage);
+app.get('/login.html', serveLoginPage);
+app.post('/login', loginUser);
+app.get('/', (req, res) => res.redirect('/todo'));
+app.get('', serveStaticFiles);
+app.get('', notFound);
+
+app.post('/saveTitle', saveTodo);
+app.delete('/deleteAllTodo', deleteTodo);
+app.post('/saveTask', saveTask);
+app.delete('/deleteTask', deleteTask);
+app.patch('/updateTaskStatus', updateTaskStatus);
+app.post('/loadTask', loadTask);
+app.post('/editTitle', editTitle);
+app.post('/editTask', editTask);
+app.post('/filterTodo', filterTodo);
+app.post('', notFound);
+
+app.use(methodNotAllowed);
+
+app.listen(port, () => stdout.write(`server listening on ${port}`));
